0d4c4bf001e8edb5257631fe58f573a232f6f853
smitraval13 <132261106+smitraval13@users.noreply.github.com>
2025-09-17 10:18:47 +0530

    17-09-2025

--- js/doubletaptogo.js
+++ js/doubletaptogo.js
@@ -2,40 +2,31 @@
 	By Osvaldas Valutis, www.osvaldas.info
 	Available for use under the MIT License
 */
-
-
-
-;(function( $, window, document, undefined )
+(function( $, window, document, undefined )
 {
 	$.fn.doubleTapToGo = function( params )
 	{
-		if( !( 'ontouchstart' in window ) &&
-			!navigator.msMaxTouchPoints &&
-			!navigator.userAgent.toLowerCase().match( /windows phone os 7/i ) ) return false;
-
+		if (!('ontouchstart' in window) && !navigator.msMaxTouchPoints && !navigator.userAgent.toLowerCase().match(/windows phone os 7/i))
+			return false;
 		this.each( function()
 		{
 			var curItem = false;
-
-			$( this ).on( 'click', function( e )
+			jQuery( this ).on( 'click', function( e )
 			{
-				var item = $( this );
+				var item = jQuery( this );
 				if( item[ 0 ] != curItem[ 0 ] )
 				{
 					e.preventDefault();
 					curItem = item;
 				}
 			});
-
-			$( document ).on( 'click touchstart MSPointerDown', function( e )
+			jQuery( document ).on( 'click touchstart MSPointerDown', function( e )
 			{
 				var resetItem = true,
-					parents	  = $( e.target ).parents();
-
+				parents	  = jQuery( e.target ).parents();
 				for( var i = 0; i < parents.length; i++ )
 					if( parents[ i ] == curItem[ 0 ] )
 						resetItem = false;
-
 				if( resetItem )
 					curItem = false;
 			});
